518a2f19688a135b226155dce404903c02f016b2
Morg92b <morgan.bouaziz@outlook.fr>
2026-07-17 17:48:21 +0200

    fix

--- Javascript_advanced/100-stock.js
+++ Javascript_advanced/100-stock.js
@@ -1,14 +1,11 @@
-const prompt = require("prompt-sync")();
-
-const stock = {
+let stock = {
   macbook: 2,
   iphone: 4,
 };
 
 function processPayment(itemName) {
-  const key = itemName.toLowerCase();
-  stock[key] -= 1;
-  console.log(`Payment is being processed for item ${itemName}`);
+  console.log(`No more ${itemName} in stock`);
+  console.log("Payment is not being processed");
 }
 
 function processError(itemName) {
@@ -18,19 +15,14 @@ function processError(itemName) {
 
 function processOrder(itemName, callbackPayment, callbackError) {
   console.log(`Verifying the stock of ${itemName}`);
-
-  const key = itemName.toLowerCase();
-  const isInStock =
-    Object.prototype.hasOwnProperty.call(stock, key) && stock[key] > 0;
-
-  if (isInStock) {
+  if (itemName > 0) {
     callbackPayment(itemName);
   } else {
     callbackError(itemName);
   }
 }
 
-const itemName = prompt(
+let item = prompt(
   "Please enter the item you would like to purchase (Macbook, iPhone)",
 );
-processOrder(itemName, processPayment, processError);
+processOrder(item, processPayment, processError);
